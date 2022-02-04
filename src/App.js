2fc2a84ed1e3d5247e901559fc459dabb7f46679
Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2022-02-05 00:10:59 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+import { Badge, Button, Col, Container, Form, ListGroup, Row } from 'react-bootstrap';
+
+function App() {
+  const [number, setNumber] = useState(1)
+  const [names, setNames] = useState('')
+  const [arrNames, setArrNames] = useState([])
+  const [groups, setGroups] = useState([])
+
+  const changeNumber = e => setNumber(e.target.value)
+  const changeNames = e => setNames(e.target.value)
+
+  const resetNames = () => {
+    setNumber(1)
+    setNames('')
+    setArrNames([])
+  }
+
+  const acak = e => {
+    e.preventDefault();
+    const grp = []
+    const data = names.split('\n').filter(item => item !== '')
+    setArrNames(names.split('\n').filter(item => item !== ''))
+
+    let activeGrp = 0
+    while (data.length > 0) {
+      const d = data.shift()
+      grp[activeGrp] = grp[activeGrp] ?? []
+      grp[activeGrp].push(d)
+
+      if (activeGrp < (number - 1)) {
+        activeGrp++
+      } else {
+        activeGrp = 0
+      }
+    }
+    setGroups(grp)
+
+    console.log(grp)
+  }
+
+  return (
+    <Container>
+      <Row className='my-5'>
+        <Col className='p-5'>
+          <Form onSubmit={acak}>
+            <Form.Group className='mb-3'>
+              <Form.Label>Jumlah Kelompok</Form.Label>
+              <Form.Control type="number" value={number} onChange={changeNumber} placeholder="Masukkan jumlah kelompok" min='1' />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Nama Anggota</Form.Label>
+              <Form.Control as="textarea" value={names} onChange={changeNames} placeholder="Masukkan nama anggota" style={{ height: '200px' }} />
+              <Form.Text className="text-muted">
+                Pisahkan nama dengan enter (baris baru)
+              </Form.Text>
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className='me-3'>
+              ACAK
+            </Button>
+            <Button variant="secondary" type="button" onClick={resetNames}>
+              RESET
+            </Button>
+          </Form>
+        </Col>
+        <Col className='p-5'>
+          <h5>Daftar Anggota :</h5>
+          {arrNames.length > 0 ? arrNames.map((item, index) => (
+            <Badge key={index} bg='secondary' className='me-2'>{item}</Badge>
+          )) : '-'}
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        {groups.length > 0 ? groups.map((item, index) => (
+          <Col key={index}>
+            <ListGroup>
+              <ListGroup.Item className='fw-bold'>Kelompok { index + 1 }</ListGroup.Item>
+              {item.map((name, i) => (
+                <ListGroup.Item key={i}>{ i + 1 }. { name }</ListGroup.Item>
+              ))}
+            </ListGroup>
+          </Col>
+        )) : <Col className='text-center'>Belum ada data</Col>}
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
