ee143219eef5732cb47a7de2a026ea993ee67dfd
Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2022-02-05 00:45:26 +0700

    .

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
   return (
     <Container>
       <Row className='my-5'>
-        <Col className='p-5'>
+        <Col sm='12' md='6' className='p-5'>
           <Form onSubmit={acak}>
             <Form.Group className='mb-3'>
               <Form.Label>Jumlah Kelompok</Form.Label>
@@ -66,7 +66,7 @@ function App() {
             </Button>
           </Form>
         </Col>
-        <Col className='p-5'>
+        <Col sm='12' md='6' className='p-5'>
           <h5>Daftar Anggota :</h5>
           {arrNames.length > 0 ? arrNames.map((item, index) => (
             <Badge key={index} bg='secondary' className='me-2'>{item}</Badge>
